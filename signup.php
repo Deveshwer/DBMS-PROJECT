e5de515e797873b362b85b82ba21d18ad3d43f70
Deveshwer <86511199+Deveshwer@users.noreply.github.com>
2022-05-28 17:04:53 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,26 +4,26 @@ session_start();
     include("connection.php");
     include("functions.php");
 
-//    if($_SERVER['REQUEST_METHOD'] == "POST")
-//    {
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
         //SOMETHING IS POSTED
-//        $user_name = $_POST['user_name'];
-//        $password = $_POST['password'];
+        $user_name = $_POST['user_name'];
+        $password = $_POST['password'];
 
-//        if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
-//        {
-//            //saving to database
-//            $user_id = random_num(20);
-//            $query = "insert into users (user_id,user_name,password) values ( '$user_id','$user_name','$password')"; 
-//            mysqli_query($con,$query);
-//            header("Location: login.php");
-//            die;
+        if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+        {
+            //saving to database
+            $user_id = random_num(20);
+            $query = "insert into users (user_id,user_name,password) values ('$user_id','$user_name','$password')"; 
+            mysqli_query($con,$query);
+            header("Location: login.php");
+            die;
         
-//        }
-//        else{
-//            echo "Please enter valid details";
-//        }
-//    }
+        }
+        else{
+            echo "Please enter valid details";
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
