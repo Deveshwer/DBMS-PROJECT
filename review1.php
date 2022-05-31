bfe1be7b1b27d26c53f4fdef5dd62ea53ae695fb
Deveshwer <86511199+Deveshwer@users.noreply.github.com>
2022-05-31 11:50:10 +0530

    Update review1.php

--- review1.php
+++ review1.php
@@ -5,21 +5,21 @@ include("functions.php");
 // If file upload form is submitted
  
 // Display status message 
-if(isset($_POST["submit1"])) {
+ if(isset($_POST["submit1"])) {
     //Process the image that is uploaded by the user
         $imagename = mysqli_real_escape_string($con,$_FILES["image"]["name"]);
         $imagedata = mysqli_real_escape_string($con,file_get_contents($_FILES["image"]["tmp_name"]));
         $imageType = mysqli_real_escape_string($con,$_FILES["image"]["type"]);
         if(substr($imageType,0,5) == "image")
         {
-            echo $imagedata;
-            mysqli_query($con,"insert into review_content(image) values('$imagedata')");
+          //  echo $imagedata;
+      //      mysqli_query($con,"insert into review_content(image) values('$imagedata')");
         }
         else
         {
             echo "Only images are allowed";
         }
-}
+}   
 $Product_name = $_POST['pname'];
 $brand = $_POST['bname'];
 $Category_name = $_POST['sel_cat'];
@@ -29,18 +29,18 @@ if(!empty($Product_name) && !empty($brand) && !empty($Category_name) && !empty($
 {
     $Product_id = random_num(20);
     //$v = $_POST['Image'];
-    $query = "insert into review_content (Product_id,Product_name,brand,Category_name,Description,Rating) values ('$Product_id','$Product_name','$brand','$Category_name','$Description','$Rating')";
-    echo $query;
+    $query = "insert into review_content (Product_id,Product_name,brand,Category_name,Description,Rating,image) values ('$Product_id','$Product_name','$brand','$Category_name','$Description','$Rating','$imagedata')";
+  //  echo $query;
     if(mysqli_query($con,$query))
-        echo "Successful";
+        echo "Successful, Your review has been uploaded!";
     else
         echo "Unsuccessful";
     mysqli_close($con);
 //    header("Location: review.php");
 }
-echo $Product_name;
-echo $brand;
-echo $Category_name;
-echo $Description;
-echo $Rating;
+//echo $Product_name;
+//echo $brand;
+//echo $Category_name;
+//echo $Description;
+//echo $Rating;
 ?>
